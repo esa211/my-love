ff09c48bb73d99ebbf79e8ad6f8def7db6354e5f
esa211 <esahlmtssdyh@gmail.com>
2025-01-12 19:57:34 +0700

    Delete iki directory

--- iki/script.js
+++
@@ -1,6 +0,0 @@
-// Fungsi untuk menampilkan pesan spesial di halaman home
-function showMessage() {
-    const messageElement = document.getElementById('message');
-    messageElement.textContent = "Aku sangat mencintaimu! ❤️";
-    messageElement.style.color = '#ff4081';
-}
